bc88ae498bdbc7e87bd40917256f417ed81bc140
Gustavo-tech <gustavosh31@gmail.com>
2021-07-21 20:42:15 -0300

    Adding loading to account page

--- src/pages/PersonalInfo/PersonalInfo.tsx
+++ src/pages/PersonalInfo/PersonalInfo.tsx
@@ -4,10 +4,11 @@ import Form from 'react-bootstrap/Form';
 import {
   Page,
   PageTitle,
-  Description
+  Description,
+  SpinnerDiv
 } from './styles';
 import { Button, Container } from '@material-ui/core';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row, Spinner } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 import { getUserInfo } from '../../client/account/get';
 import { useReactOidc } from '@axa-fr/react-oidc-context';
@@ -18,72 +19,82 @@ const PersonalInfo = () => {
 
   const [accountCreationDate, setAccountCreationDate] = useState('');
   const [cpf, setCpf] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const email = profile.email;
   useEffect(() => {
+    setLoading(true)
     getUserInfo('Bearer ' + access_token, email, (data) => {
       setAccountCreationDate(new Date(data.accountCreationDate).toDateString());
       setCpf(data.cpf);
+      setLoading(false);
     })
   }, [])
+
+  const content = loading ? (
+    <SpinnerDiv>
+      <Spinner animation="border" variant="danger" />
+    </SpinnerDiv>
+  ) : (
+    <div>
+      <PageTitle>Account information</PageTitle>
+      <Description>Your account basic information</Description>
+      <Container
+        style={{
+          backgroundColor: 'var(--red)',
+          padding: '2%',
+          width: '80%',
+          borderRadius: '12px'
+        }}
+      >
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label style={{ color: 'var(--white)' }}>CPF</Form.Label>
+              <Form.Control disabled value={cpf} />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label style={{ color: 'var(--white)' }}>Phone</Form.Label>
+              <Form.Control disabled value={profile.phone_number} />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label style={{ color: 'var(--white)' }}>Email</Form.Label>
+              <Form.Control disabled value={profile.email} />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label style={{ color: 'var(--white)' }}>UserName</Form.Label>
+              <Form.Control disabled value={profile.name} />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label style={{ color: 'var(--white)' }}>Account Creation Date</Form.Label>
+              <Form.Control disabled value={accountCreationDate} />
+            </Form.Group>
+
+            <Button
+              style={{
+                backgroundColor: 'var(--black)',
+                color: 'var(--white)',
+                float: 'right'
+              }}
+            >
+              Edit
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
 
   return (
     <>
       <Navbar />
       <Page>
         <SidebarSettings />
-        <div>
-          <PageTitle>Account information</PageTitle>
-          <Description>Your account basic information</Description>
-          <Container
-            style={{
-              backgroundColor: 'var(--red)',
-              padding: '2%',
-              width: '80%',
-              borderRadius: '12px'
-            }}
-          >
-            <Row>
-              <Col>
-                <Form.Group>
-                  <Form.Label style={{ color: 'var(--white)' }}>CPF</Form.Label>
-                  <Form.Control disabled value={cpf} />
-                </Form.Group>
-
-                <Form.Group>
-                  <Form.Label style={{ color: 'var(--white)' }}>Phone</Form.Label>
-                  <Form.Control disabled value={profile.phone_number} />
-                </Form.Group>
-
-                <Form.Group>
-                  <Form.Label style={{ color: 'var(--white)' }}>Email</Form.Label>
-                  <Form.Control disabled value={profile.email} />
-                </Form.Group>
-
-                <Form.Group>
-                  <Form.Label style={{ color: 'var(--white)' }}>UserName</Form.Label>
-                  <Form.Control disabled value={profile.name} />
-                </Form.Group>
-
-                <Form.Group>
-                  <Form.Label style={{ color: 'var(--white)' }}>Account Creation Date</Form.Label>
-                  <Form.Control disabled value={accountCreationDate} />
-                </Form.Group>
-
-                <Button
-                  style={{
-                    backgroundColor: 'var(--black)',
-                    color: 'var(--white)',
-                    float: 'right'
-                  }}
-                >
-                  Edit
-                </Button>
-              </Col>
-            </Row>
-          </Container>
-
-        </div>
+        {content}
       </Page>
     </>
   )
--- src/pages/PersonalInfo/styles.ts
+++ src/pages/PersonalInfo/styles.ts
@@ -14,8 +14,13 @@ export const Page = styled.div`
   padding: 2% 0;
   display: grid;
   grid-template-columns: 20% 80%;
-  background-color: var(--white);
   grid-template-rows: 100%;
   width: 100%;
   height: 100%;
+`
+
+export const SpinnerDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
