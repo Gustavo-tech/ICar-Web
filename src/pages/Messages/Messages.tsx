d95e47b675f0112c13cf8a4a56323748809868c8
Gustavo-tech <gustavosh31@gmail.com>
2021-09-12 17:48:57 -0300

    Adding profile sidebar

--- src/pages/Messages/Messages.tsx
+++ src/pages/Messages/Messages.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import AppNavbar from '../../components/Navbar/Navbar'
 import TalkSidebar from '../../components/Sidebars/TalkSidebar/TalkSidebar'
-import { Body, TalkHeader, TalkHeaderTitle } from './styles'
+import {
+  Body,
+  DetailsWrapper,
+  Message,
+  NickName,
+  TalkBody,
+  TalkHeader,
+  TalkHeaderTitle,
+  TalkWrapper,
+  UserPic
+} from './styles'
 
 const Messages = () => {
   return (
@@ -9,9 +19,19 @@ const Messages = () => {
       <AppNavbar />
       <Body>
         <TalkSidebar />
-        <TalkHeader>
-          <TalkHeaderTitle>ICar</TalkHeaderTitle>
-        </TalkHeader>
+        <TalkWrapper>
+          <TalkHeader>
+            <TalkHeaderTitle>ICar</TalkHeaderTitle>
+          </TalkHeader>
+          <TalkBody>
+            <Message sent={false}>Hello</Message>
+            <Message sent={true}>Hello</Message>
+          </TalkBody>
+        </TalkWrapper>
+        <DetailsWrapper>
+          <UserPic>GH</UserPic>
+          <NickName>Gustavo Henrique</NickName>
+        </DetailsWrapper>
       </Body>
     </>
   )
--- src/pages/Messages/styles.ts
+++ src/pages/Messages/styles.ts
@@ -8,13 +8,22 @@ export const Body = styled.div`
   margin-top: 3%;
 `
 
+export const TalkWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  width: 53%;
+  height: 100%;
+`
+
 export const TalkHeader = styled.header`
   display: flex;
   justify-content: flex-start;
   align-items: center;
   background-color: white;
   border: 1px solid #B5B5B5;
-  width: 55%;
+  width: 100%;
   height: 13%;
   padding: 0 3%;
 `
@@ -22,4 +31,57 @@ export const TalkHeader = styled.header`
 export const TalkHeaderTitle = styled.h2`
   color: var(--red);
   font-family: 'Satisfy', cursive;
+`
+
+export const TalkBody = styled.div`
+  background-color: transparent;
+  width: 100%;
+  height: 87%;
+  border: 1px solid #B5B5B5;
+  padding: 4%;
+`
+
+export const DetailsWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  padding: 2% 0;
+  width: 22%;
+  height: 100%;
+  background-color: white;
+  border: 1px solid #B5B5B5;
+`
+
+export const UserPic = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 35%;
+  border-radius: 50%;
+  width: 60%;
+  margin-bottom: 6%;
+`
+
+export const NickName = styled.h3`
+  text-align: center;
+  font-family: 'Satisfy', cursive;
+  font-size: 4vh;
+`
+
+interface MessageProps {
+  sent: boolean;
+}
+
+export const Message = styled.div<MessageProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => props.sent ? 'var(--red)' : 'var(--gray)'};
+  justify-self: ${props => props.sent ? 'flex-start' : 'flex-end'};
+  margin-bottom: 4%;
+  flex-wrap: wrap;
+  color: white;
+  width: 20%;
+  height: 17%;
 `
