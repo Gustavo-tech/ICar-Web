4942c0e9d1d7a47fc717a63fae6053921ba548a9
Gustavo-tech <gustavosh31@gmail.com>
2022-01-11 14:33:50 -0300

    Small update

--- src/configurations/open-id.ts
+++ src/configurations/open-id.ts
@@ -1,8 +1,8 @@
 export const clientConfig = {
   response_type: 'code',
-  client_id: 'bf5247f2-8edc-47a2-8ded-5cd7789f3c1a',
-  authority: 'https://icarorg.b2clogin.com/icarorg.onmicrosoft.com/b2c_1_icar/v2.0/.well-known/openid-configuration',
+  client_id: '',
+  authority: '',
   redirect_uri: 'http://localhost:3000/authentication/callback',
-  scope: 'https://icarorg.onmicrosoft.com/8bb8359a-7a4c-4436-8a49-ccb215a87581/all',
+  scope: '',
   silent_redirect_uri: 'http://localhost:3000/authentication/silent_callback'
 }
